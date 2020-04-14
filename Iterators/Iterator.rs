7d5c0a8778a66b0996bdfd699534777182ed4e94
¨Gober¨ <reyesfragosoroberto@gmail.com>
2020-04-13 21:10:03 -0500

    [Added] interator of elements and indexs

--- Iterators/Iterator.rs
+++ Iterators/Iterator.rs
@@ -8,11 +8,16 @@ fn main(){
                 "Pakistan",
                 "Burma",
             ];
-        // Don't get the searched item but rather its index
+            
+    // Don't get the searched item but rather its index
     if let Some(pos) = countries
     .iter()
     .position(|country| country.starts_with('I'))
     {
         println!("It's index is: {}", pos);
     }
+    
+    // Creates an iterator which gives the current iteration count as well as the next value.
+    for (count, elem) in countries.iter().enumerate(){
+    } 
 }
